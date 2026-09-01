5257b3df2c209d5e44867d348c7d44176fdea1e2
YahhoWorld <taikikimura07090709@gmail.com>
2026-09-01 12:16:20 +0900

    テキストと画像のjavaコードからの変更

--- app/src/main/java/jp/ac/meijou/android/s251205059/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205059/MainActivity.java
@@ -8,17 +8,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s251205059.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        TextView textView=findViewById(R.id.text_view);
+//        textView.setText(R.string.text);
+        textView.setText(R.string.text);
+        ImageView imageView=findViewById(R.id.imageView);
+//        imageView.setImageResource(R.drawable.outline_accessible_forward_24);
+        imageView.setImageResource(R.drawable.outline_accessible_forward_24);
     }
+
+
 }
