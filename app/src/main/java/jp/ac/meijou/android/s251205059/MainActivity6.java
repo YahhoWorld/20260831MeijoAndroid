37d0a0a920cb51d86625229ae023fdc07ee43875
YahhoWorld <taikikimura07090709@gmail.com>
2026-09-03 14:01:51 +0900

    net ex view

---
+++ app/src/main/java/jp/ac/meijou/android/s251205059/MainActivity6.java
@@ -0,0 +1,30 @@
+package jp.ac.meijou.android.s251205059;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.s251205059.databinding.ActivityMain6Binding;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    private ActivityMain6Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main6);
+        binding=ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
